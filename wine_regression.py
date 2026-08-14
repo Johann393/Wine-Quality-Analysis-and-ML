2f36cccd7b6741b5719b1e913db56b1b169141fd
Thomas Johann Hillermann Gomes <johannhillermann393@gmail.com>
2026-08-14 16:46:18 -0300

    Update wine_regression.py

--- wine_regression.py
+++ wine_regression.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-df1 = pd.read_csv("data/winequality-red.csv", sep=";")
-df2 = pd.read_csv("data/winequality-white.csv", sep=";")
+df1 = pd.read_csv("wine dataset/winequality-red.csv", sep=";")
+df2 = pd.read_csv("wine dataset/winequality-white.csv", sep=";")
 
 df = pd.concat([df1, df2], ignore_index=True)
 
